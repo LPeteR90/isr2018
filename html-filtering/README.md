ae3e15f0ea47f5b99b9740a221635873b99b146a
Peter Grassberger <petertheone@gmail.com>
2018-10-14 22:36:21 +0200

    html-filtering: add search and meta info

--- html-filtering/README.md
+++ html-filtering/README.md
@@ -6,16 +6,20 @@ a python program that removes HTML tags from an input file or url.
 usage
 -----
 
-`python3 html-filtering.py test.html`
+`python3 html-filtering.py test.html world`
 
 or
 
-`python3 html-filtering.py https://orf.at`
+`python3 html-filtering.py https://orf.at EU`
 
 output for test.html
 --------------------
 
+```
 file detected, reading...
+title: Hello, world!
+keywords: HTML,CSS,XML,JavaScript
+description: Free Web tutorials
 filtering html tags...
 result:
 
@@ -24,14 +28,16 @@ result:
     
     
     
+    
+    
 
     
     
 
-    Hello, world!
+    Hello, <<world>>!
   
   
-    Hello, world!
+    Hello, <<world>>!
 
     
     
@@ -42,3 +48,4 @@ result:
 
 
 
+```
--- html-filtering/html-filtering.py
+++ html-filtering/html-filtering.py
@@ -19,14 +19,38 @@ def get_data(file_or_url):
             data = file.read()
     return data
 
+def print_metadata(data):
+    regex_strings = {
+        'title': '<title>(?P<title>.*)</title>',
+        # todo: improve that attributes don't need to be in that order.
+        'keywords': '<meta\s+name="keywords"\s+content="(?P<keywords>.*)"\s*(\/)?>',
+        'description': '<meta\s+name="description"\s+content="(?P<description>.*)"\s*(\/)?>'
+    }
+
+    for name, regex in regex_strings.items():
+        value = re.search(regex, data)
+        if value is None:
+            value = ''
+        else:
+            value = value.group(name)
+        print('{}: {}'.format(name, value))
+
+
+def mark_search_term(data, search_term):
+    if search_term == '':
+        return data
+    return re.sub(search_term, '<<{}>>'.format(search_term), data)
+
 def replace_html_tags(data):
     print('filtering html tags...')
     html_tags_regex = re.compile('<(\/)?.*?(\/)?>', re.DOTALL)
     return re.sub(html_tags_regex, '', data)
 
-def html_filtering(input_file_or_url):
+def html_filtering(input_file_or_url, search_term=''):
     data = get_data(input_file_or_url)
+    print_metadata(data)
     data = replace_html_tags(data)
+    data = mark_search_term(data, search_term)
 
     print('result:')
     print(data)
@@ -37,8 +61,9 @@ def html_filtering(input_file_or_url):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file_or_url')
+    parser.add_argument('search_term') # todo: make this argument optional
     args = parser.parse_args()
-    html_filtering(args.input_file_or_url)
+    html_filtering(args.input_file_or_url, args.search_term)
 
 if __name__ == '__main__':
     main()
